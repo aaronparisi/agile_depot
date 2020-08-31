de62bdb64bdf67e2cdcb79f2d4f1ffd969ce19a0
Aaron <parisi.aaron@gmail.com>
2020-08-31 15:34:09 -0700

    model tests for price, attribute inclusion, image_url extension

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,31 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
+  }
+
 end
+
+# ? why can I create new products in the rails console with negative prices?
+# because the model validations are not run until you try to save the
+# data in the databse
+# ? why does price: 1 result in price: 0.1e1?
+# 0.1 x 10^1 = 1
+# ? why doesn't the image_url validation fail when I fuck w the regex??
+# because you defined the test INSIDE the new_product() method
+# so the tests didn't even run
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,68 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(
+      title:       "My Book Title",
+      description: "yyy",
+      image_url:   "zzz.jpg"
+    )
+    product.price = -1
+
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(
+      title:       "My Book Title",
+      description: "yyy",
+      price:       1,
+      image_url:   image_url
+    )
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    notok = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |url|
+      assert new_product(url).valid?, "#{url} should be valid"
+    end
+    
+    notok.each do |url|
+      assert new_product(url).invalid?, "#{url} shouldn't be valid"
+    end      
+  end
 end
