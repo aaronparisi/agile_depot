f06b7b0affd9f4c1417dc87ec38982839e837dda
Aaron <parisi.aaron@gmail.com>
2020-09-02 13:45:50 -0700

    store controller functional tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,15 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    # there should be at least 4 anchors desc from: (id="side" desc from: (id="columns"))
+    # these correspond to the 4 links in the sidebar
+    assert_select '#main .entry', 3
+    # there should be 3 entries (our 3 products)
+    assert_select 'h3', 'Lord of the Rings, The Two Towers'
+    assert_select '.price', /\$[,\d]+\.\d\d/
+    # anything with class=price should match that format: '$' followed by 
+    # digits and commas followed by '.' followed by 2 digits
   end
 
 end
