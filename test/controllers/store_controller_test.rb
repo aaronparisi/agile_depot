17e9528295965e101ad1106a04116cf86657dfc0
Aaron <parisi.aaron@gmail.com>
2020-09-03 13:31:45 -0700

    added date and time to application sidebar

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -8,7 +8,9 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     # there should be at least 4 anchors desc from: (id="side" desc from: (id="columns"))
     # these correspond to the 4 links in the sidebar
     assert_select '#main .entry', 3
-    # there should be 3 entries (our 3 products)
+    # there should be 3 entries (our 3 products in the products.yml file)
+    # remember we are running tests using the database data,
+    # we are using a separate set of data
     assert_select 'h3', 'Lord of the Rings, The Two Towers'
     assert_select '.price', /\$[,\d]+\.\d\d/
     # anything with class=price should match that format: '$' followed by 
