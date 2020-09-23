a8f1585cd5e3d42102aca34c95982f137fdd4f74
Aaron <parisi.aaron@gmail.com>
2020-09-23 09:37:47 -0700

    restrict access to all but store, carts, and line items via application controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,13 +1,9 @@
 class AdminController < ApplicationController
+
   def index
     @order_count = Order.count
     @unshipped = Order.where(ship_date: nil).length
-    sesh_id = session[:user_id]
-    if sesh_id
-      @adminUser = User.find(session[:user_id])
-    else
-      redirect_to login_url, notice: "You must be logged in to access admin pages"
-    end
+    @adminUser = User.find(session[:user_id])
   end  
   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,27 @@
+require 'byebug'
 class ApplicationController < ActionController::Base
 # rescue_from ActiveRecord::RecordNotFound, with: :email_admin
 # rescue_from ActiveRecord::ActiveRecordError, with: :email_admin
 # rescue_from StandardError, with: :email_admin
 
+  before_action :authorize
+  AUTHORIZE_CONTROLLER_EXCEPTIONS = ['store', 'carts', 'line_items']
+
 # private
 
-def email_admin(e)
-  ErrorMailer.error_thrown(e.message).deliver_later
-  render 'shared/mistake'
-end
+  def email_admin(e)
+    ErrorMailer.error_thrown(e.message).deliver_later
+    render 'shared/mistake'
+  end
+
+  protected
+
+  def authorize
+    unless AUTHORIZE_CONTROLLER_EXCEPTIONS.include?(params[:controller])
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "You must be logged in to access admin pages"
+      end
+    end
+  end
 
 end
